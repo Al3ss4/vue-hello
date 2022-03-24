53020dc22d31202dfd879077eeca88e7fb8e43e4
Mario Rovito <alessandro.r14@gmail.com>
2022-03-24 15:08:25 +0100

    add html and js code

---
+++ script.js
@@ -0,0 +1,11 @@
+console.log('JS VUE OK!');
+
+/*
+Descrizione:
+Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+*/
+
+/*
+Bonus:
+Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+*/
