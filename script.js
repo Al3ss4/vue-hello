dd634da3210f08de1f77ec8c40aefa88fa1dee11
Mario Rovito <alessandro.r14@gmail.com>
2022-03-24 15:16:33 +0100

    add text and color

--- script.js
+++ script.js
@@ -9,3 +9,16 @@ Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */
+
+const root = new Vue ({
+
+    el : '#root',
+
+    //aggiungo testo e css
+    data : {
+        messaggio : "vue-hello!",
+        classe : "color", 
+       
+    }
+    
+});
